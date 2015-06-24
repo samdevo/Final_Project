4dc2b6089d1d55b9f20fbd8135ec0e02c0e1a618
Viraj Kathuria <vkathuria.2020@saintannsny.org>
2015-06-24 18:24:32 +0000

    merge conflict

--- country-code.rb
+++ country-code.rb
@@ -1,6 +1,28 @@
 def find_country(country)
   if countries.incude?(country)
-    
+    @country + ":"
   else 
     "Your country is invalid sorry:("
 end 
+  @country={
+    :China
+    :India
+    :United_States_of_America
+    :Indonesia
+    :Brazil
+    :Pakistan
+    :Nigeria
+    :Bangledesh
+    :Russia
+    :Japan 
+    :Mexico
+    :Philippines
+    :Vietnam
+    :Ethiopia
+    :Egypt
+    :Germany
+    :Iran
+    :Turkey
+    :Democratic_Republic_of_the_Congo
+    
+    }
