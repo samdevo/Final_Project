2e9b6aab6718cec5a6ce7eca3438e67ac30794a9
Viraj Kathuria <vkathuria.2020@saintannsny.org>
2015-06-24 18:43:01 +0000

    fix merge

---
+++ models/country-code.rb
@@ -0,0 +1,28 @@
+def find_country(country)
+  if countries.incude?(country)
+    @country + ":"
+  else 
+    "Your country is invalid sorry:("
+end 
+  @country={
+    :China => ["1,370,570,000"]
+    :India => ["1,273,140,000"]
+    :United_States_of_America => ["321,268,000"]
+    :Indonesia => ["255,461,700"]
+    :Brazil => ["204,503,000"]
+    :Pakistan => ["190,156,000"]
+    :Nigeria => ["183,523,000"]
+    :Bangledesh => ["158,560,000"]
+    :Russia => ["146,267,288"]
+    :Japan => ["126,880,000"]
+    :Mexico => ["121,005,815"]
+    :Philippines => ["101,642,800"]
+    :Vietnam => ["91,583,000"]
+    :Ethiopia => ["90,076,012"]
+    :Egypt => ["88,741,500"]
+    :Germany => ["81,083,600"]
+    :Iran => ["78,435,100"]
+    :Turkey => ["77,695,904"]
+    :Democratic_Republic_of_the_Congo => ["71,246,000"]
+    
+    }
