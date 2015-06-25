840f662c801ddeae43c813c35b48b4658c1bd59b
Viraj Kathuria <vkathuria.2020@saintannsny.org>
2015-06-25 18:04:42 +0000

    Updated Country-code

--- models/country-code.rb
+++ models/country-code.rb
@@ -62,7 +62,7 @@ class Countries
    @fail
    @country[@chosencountry]
   end
-  end
+  
 
   def find_country(country)
     if countries.incude?(country)
@@ -73,27 +73,4 @@ class Countries
 
     end 
  end 
-  @country={
-    :China => ["1,370,570,000", "Chinese", "Beijing"],
-    :India => ["1,273,140,000", "Hindi and English", "New Delhi"],
-    :United_States_of_America => ["321,268,000", "English", "Washington D.C."],
-    :Indonesia => ["255,461,700", "Indonesian", "Jakarta"],
-    :Brazil => ["204,503,000", "Portuguese", "Brasília"],
-    :Pakistan => ["190,156,000", "Urdu", "Islamabad"],
-    :Nigeria => ["183,523,000", "English", "Abuja"],
-    :Bangledesh => ["158,560,000", "Bengali", "Dhaka"],
-    :Russia => ["146,267,288", "Russian", "Moscow"],
-    :Japan => ["126,880,000", "Japanese", "Tokyo"],
-    :Mexico => ["121,005,815", "Mexican", "Mexico City"],
-    :Philippines => ["101,642,800", "Filipino", "Manila"],
-    :Vietnam => ["91,583,000", "Vietnamese", "Hanoi"],
-    :Ethiopia => ["90,076,012", "Does not have an offical language.", "Addis Ababa"],
-    :Egypt => ["88,741,500", "Arabic", "Cairo"],
-    :Germany => ["81,083,600", "German", "Berlin"],
-    :Iran => ["78,435,100", "Persian", "Tehran"],
-    :Turkey => ["77,695,904", "Turkish", "Ankara"],
-    :Democratic_Republic_of_the_Congo => ["71,246,000", "French", "Kinshasa"]
-    
-    
-    }
 end 
